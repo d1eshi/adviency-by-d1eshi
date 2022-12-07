1dbee902d0e2388a09aadaca569ec1fef55c4001
Diego Silva <silva.diegof03@gmail.com>
2022-12-07 10:04:14 -0300

    day 2. basic styels

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 export default function App() {
   return (
     <div className='App'>
-      <h1>Regalos:</h1>
-      <ul>
-        <li>Medias</li>
-        <li>Gorras</li>
-        <li>Tuki</li>
-      </ul>
+      <div className=' container'>
+        <h1 className='title-1'>Regalos:</h1>
+        <ul className='listPresents'>
+          <li>Medias</li>
+          <li>Gorras</li>
+          <li>Tuki</li>
+        </ul>
+      </div>
     </div>
   )
 }
